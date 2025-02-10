b2e6f18fb590ffdb32238903157256fac5b4bc0b
stevenbenalcazar <sabenalcazarm@uce.edu.ec>
2025-02-09 19:35:44 -0500

    ojala ya sirva

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,20 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Cargar variables de entorno
-export default defineConfig(({ mode }) => {
-  // Carga las variables de entorno desde el archivo .env
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api/auth': {
-          target: env.VITE_LOGIN_SERVICE_URL || 'http://54.235.29.178:3000',
-          changeOrigin: true,
-          secure: false,
-        },
-        '/api/users': {
-          target: env.VITE_USER_SERVICE_URL || 'http://54.167.50.122:3000',
-          changeOrigin: true,
-          secure: false,
-        },
-        '/api/workspace': {
-          target: env.VITE_WORKSPACE_SERVICE_URL || 'http://XX.XX.XX.XX:3000',
-          changeOrigin: true,
-          secure: false,
-        },
-        '/api/tasks': {
-          target: env.VITE_TASKS_SERVICE_URL || 'http://XX.XX.XX.XX:3000',
-          changeOrigin: true,
-          secure: false,
-        },
-        '/api/notifications': {
-          target: env.VITE_NOTIFICATIONS_SERVICE_URL || 'http://XX.XX.XX.XX:3000',
-          changeOrigin: true,
-          secure: false,
-        },
-        '/api/calendar': {
-          target: env.VITE_CALENDAR_SERVICE_URL || 'http://XX.XX.XX.XX:3000',
-          changeOrigin: true,
-          secure: false,
-        },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api/auth': {
+        target: 'http://34.234.86.131:3000', // LOGIN-SERVICE
+        changeOrigin: true,
+        secure: false,
+      },
+      '/api/users': {
+        target: 'http://54.167.171.189:3000', // USER-SERVICE
+        changeOrigin: true,
+        secure: false,
       },
     },
-  };
+  },
 });
